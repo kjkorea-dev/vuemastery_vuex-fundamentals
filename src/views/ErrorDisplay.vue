6110432d28dac0ac39feca6726a933bef5d2a887
kj <agassi77@gmail.com>
2021-08-23 15:04:54 +0900

    Error Handling : implement error handling

---
+++ src/views/ErrorDisplay.vue
@@ -0,0 +1,15 @@
+<template>
+  <h4>Oops! There was an error:</h4>
+  <p>{{ error }}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
